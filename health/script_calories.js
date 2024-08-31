8ef7e31e730159a8547a0ceb4a3d713ead790f30
Sepehr Akbari <isepehrakbari@gmail.com>
2024-08-31 15:05:45 -0500

    updating the colors

--- health/script_calories.js
+++ health/script_calories.js
@@ -1,4 +1,5 @@
-const gradeSpan = document.querySelector('span.displayNG'); // Get the span element for calorie display
+const calorieSpan = document.querySelector('span.displayC'); // Get the span element for calorie display
+const bmiSpan = document.querySelector('span.displayB'); // Get the span element for bmi display
 const genderSelect = document.querySelector('select#gender'); // Get the gender select element (you would need to add this in HTML)
 
 function calculateCalories() {
@@ -20,7 +21,29 @@ function calculateCalories() {
     }
 
     // Update the span with the calculated calories
-    gradeSpan.textContent = bmr.toFixed(0);
+    calorieSpan.textContent = bmr.toFixed(0);
+}
+
+function calculateBmi() {
+    const age = parseFloat(document.querySelector('input[placeholder="Age"]').value) || 0;
+    const height = parseFloat(document.querySelector('input[placeholder="Height(cm)"]').value) || 0;
+    const weight = parseFloat(document.querySelector('input[placeholder="Weight(kg)"]').value) || 0;
+    const gender = genderSelect.value; // Get selected gender
+
+    let bmr;
+
+    if (gender === 'male') {
+        // BMR calculation for men
+        bmr = 10 * weight + 6.25 * height - 5 * age + 5;
+    } else if (gender === 'female') {
+        // BMR calculation for women
+        bmr = 10 * weight + 6.25 * height - 5 * age - 161;
+    } else {
+        bmr = 0; // Default value if gender is not selected
+    }
+
+    // Update the span with the calculated calories
+    calorieSpan.textContent = bmr.toFixed(0);
 }
 
 // Add event listeners for buttons
